ddef198b3005bc03e2d7bc39e78e806a4a8a5cd5
bmoxey <bmoxey@gmail.com>
2023-12-02 15:57:55 +1100

    Initial Commit

---
+++ Hockey Victoria/Hockey_VictoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hockey_VictoriaApp.swift
+//  Hockey Victoria
+//
+//  Created by Brett Moxey on 2/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Hockey_VictoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
